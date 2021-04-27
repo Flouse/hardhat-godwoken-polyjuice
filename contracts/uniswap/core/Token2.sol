026b03e563afe1905f16cb390977ce88eb35e81f
Flouse <flouse@gmail.com>
2021-04-27 19:10:48 +0800

    npm install @openzeppelin/contracts
    for ERC20 token contracts

---
+++ contracts/uniswap/core/Token2.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity =0.5.16;
+
+// We import this library to be able to use console.log
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title Token 2 = TK2
+contract Token2 is ERC20Detailed, ERC20 {
+	  constructor() ERC20Detailed('Token 2', 'TK2', 18) public {}
+}
