328c78e55c4dbb736f48b4f786e0806942b79788
Flouse <flouse@gmail.com>
2021-05-02 16:40:25 +0800

    test: SimpleStorage Should be updated by other users

--- test/SimpleStorage.js
+++ test/SimpleStorage.js
@@ -24,5 +24,13 @@ describe("SimpleStorage Contract on Ethereum", function () {
 			await contract.set("1010");
 			expect(await contract.get()).to.equal(1010);
 		});
+
+		it("Should be updated by other users", async function () {
+			expect(await contract.get()).to.equal(123);
+			await contract.connect(addr1).set(1);
+			expect(await contract.get()).to.equal(1);
+			await contract.connect(addr2).set(2);
+			expect(await contract.connect(addr1).get()).to.equal(2);
+		});
 	});
 });
