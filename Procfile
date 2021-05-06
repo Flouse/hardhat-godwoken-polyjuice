6146eded5e52492ade257836c4df3c714cbf90d7
Flouse <flouse@gmail.com>
2021-05-06 17:25:30 +0800

    add nft backends and deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd backend/nft && npm start
---
+++ backend/nft/git-partial-clone.sh
@@ -0,0 +1,8 @@
+git clone \
+  --depth 1  \
+  --filter=blob:none  \
+  --no-checkout \
+  https://github.com/jklepatch/eattheblocks.git \
+;
+cd eattheblocks
+git checkout master -- screencast/318-create-nft
